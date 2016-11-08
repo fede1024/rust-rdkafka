bd85244ba9e361c314520ec63dfccd16d30a2b50
Federico Giraud <giraud.federico@gmail.com>
2016-11-07 19:40:56 -0800

    Add support for features

--- librdkafka-sys/build.rs
+++ librdkafka-sys/build.rs
@@ -5,13 +5,6 @@ use std::process::Command;
 use std::io::Write;
 use std::env;
 
-macro_rules! t {
-    ($e:expr) => (match $e{
-        Ok(e) => e,
-        Err(e) => panic!("{} failed with {}", stringify!($e), e),
-    })
-}
-
 macro_rules! println_stderr(
     ($($arg:tt)*) => { {
         let r = writeln!(&mut ::std::io::stderr(), $($arg)*);
@@ -35,16 +28,40 @@ fn main() {
     //     println!("Setting up submodules");
     //     run_command_or_fail(".", "git", &["submodule", "update", "--init"]);
     // }
+
+    let feature_sasl = env::var("CARGO_FEATURE_SASL").is_ok();
+    let feature_ssl = env::var("CARGO_FEATURE_SSL").is_ok();
+    let feature_zlib = env::var("CARGO_FEATURE_ZLIB").is_ok();
+
+    let mut configure_flags = Vec::new();
+
+    if !feature_sasl {
+        configure_flags.push("--disable-sasl");
+    }
+
+    if !feature_ssl {
+        configure_flags.push("--disable-ssl");
+    }
+
     println!("Configuring librdkafka");
-    run_command_or_fail("librdkafka", "./configure", &["--disable-sasl", "--disable-ssl"]);
+    run_command_or_fail("librdkafka", "./configure", configure_flags.as_slice());
+
     println!("Compiling librdkafka");
     run_command_or_fail("librdkafka", "make", &["-j", &num_cpus::get().to_string()]);
+
     println!("cargo:rustc-link-search=native={}/librdkafka/src", env::current_dir().expect("Can't find current dir").display());
-    println!("cargo:rustc-link-search=/usr/local/opt/openssl/lib");
     println!("cargo:rustc-link-lib=static=rdkafka");
-    println!("cargo:rustc-link-lib=dylib=crypto");
-    println!("cargo:rustc-link-lib=dylib=ssl");
-    println!("cargo:rustc-link-lib=dylib=z");
-    println!("cargo:libdir=/usr/local/opt/openssl/lib");
-    println!("cargo:include=/usr/local/opt/openssl/include");
+
+    if feature_ssl {
+        println!("cargo:rustc-link-lib=dylib=crypto");
+        println!("cargo:rustc-link-lib=dylib=ssl");
+    }
+
+    if feature_zlib {
+        println!("cargo:rustc-link-lib=dylib=z");
+    }
+
+    // println!("cargo:rustc-link-search=/usr/local/opt/openssl/lib");
+    // println!("cargo:libdir=/usr/local/opt/openssl/lib");
+    // println!("cargo:include=/usr/local/opt/openssl/include");
 }
