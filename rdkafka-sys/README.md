909122d95b8f623ebbafad88ded1e6c1da1b613a
Thijs Cadier <thijs@appsignal.com>
2017-03-14 12:34:32 +0100

    Update bindgen command and disable warnings in sys package

--- rdkafka-sys/README.md
+++ rdkafka-sys/README.md
@@ -5,5 +5,5 @@ Sys wrapper around [librdkafka](https://github.com/edenhill/librdkafka).
 To regenerate the bindings:
 
 ```
-bindgen --builtins --convert-macros librdkafka/src/rdkafka.h > src/bindings.rs
+bindgen --builtins --convert-macros librdkafka/src/rdkafka.h > src/bindings/{platform}.rs
 ```
--- rdkafka-sys/src/bindings/mod.rs
+++ rdkafka-sys/src/bindings/mod.rs
@@ -1,12 +1,15 @@
 #[cfg(target_os = "linux")]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
 mod linux;
 
 #[cfg(target_os = "macos")]
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
 mod macos;  // Temporarily use linux binding for mac as well
 
 #[cfg(not(any(target_os = "linux", target_os = "macos")))]
 panic!("Your platform is not yet supported.");
-
 
 #[cfg(target_os = "linux")]
 pub use self::linux::*;
