eef80d2f1581d878373433c52f46930c8a7edf60
Federico Giraud <giraud.federico@gmail.com>
2017-05-13 10:27:26 +0100

    Check library before building; add version check

---
+++ rdkafka-sys/tests/version_check.rs
@@ -0,0 +1,19 @@
+extern crate rdkafka_sys as rdsys;
+
+use std::ffi::CStr;
+
+const PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+fn remove_pre(version: &str) -> &str {
+    version.split('-')
+        .next()
+        .expect("Crate version is not valid")
+}
+
+#[test]
+fn check_version() {
+    let version_str_c = unsafe { CStr::from_ptr(rdsys::rd_kafka_version_str()) };
+    let rdsys_version = version_str_c.to_string_lossy();
+
+    assert_eq!(remove_pre(&rdsys_version), remove_pre(PKG_VERSION))
+}
