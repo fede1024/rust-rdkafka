5cb5ba2cb404e60321683c402b3f89bda403f0a0
Federico Giraud <giraud.federico@gmail.com>
2018-06-30 11:58:08 +0100

    Update documentation

--- theme.js
+++ theme.js
@@ -1,7 +1,6 @@
 var themes = document.getElementById("theme-choices");
 var themePicker = document.getElementById("theme-picker");
-
-function switchThemeButtonState() {
+themePicker.onclick = function() {
     if (themes.style.display === "block") {
         themes.style.display = "none";
         themePicker.style.borderBottomRightRadius = "3px";
@@ -12,28 +11,11 @@ function switchThemeButtonState() {
         themePicker.style.borderBottomLeftRadius = "0";
     }
 };
-
-function handleThemeButtonsBlur(e) {
-    var active = document.activeElement;
-    var related = e.relatedTarget;
-
-    if (active.id !== "themePicker" &&
-        (!active.parentNode || active.parentNode.id !== "theme-choices") &&
-        (!related ||
-         (related.id !== "themePicker" &&
-          (!related.parentNode || related.parentNode.id !== "theme-choices")))) {
-        switchThemeButtonState();
-    }
-}
-
-themePicker.onclick = switchThemeButtonState;
-themePicker.onblur = handleThemeButtonsBlur;
 ["dark","light"].forEach(function(item) {
     var but = document.createElement('button');
     but.innerHTML = item;
     but.onclick = function(el) {
         switchTheme(currentTheme, mainTheme, item);
     };
-    but.onblur = handleThemeButtonsBlur;
     themes.appendChild(but);
 });
