54a8d0228e473fc82a488f87b857f7dc16090452
Nikhil Benesch <nikhil.benesch@gmail.com>
2019-04-23 11:46:38 -0400

    Don't build librdkafka with --enable-static
    The --enable-static flag is a bit of a misnomer. It doesn't tell
    librdkafka to build static libraries; rather, it tells librdkafka to
    attempt to statically link liblz4, libz, et al. into the *shared*
    librdkafka. This, unsurprisingly, often goes wrong.
    
    Just drop the --enable-static flag. We'll still get a librdkafka.a that
    we can link against, and we'll let cargo link in liblz4, libz, et al.

--- rdkafka-sys/build.rs
+++ rdkafka-sys/build.rs
@@ -105,8 +105,6 @@ fn build_librdkafka() {
         configure_flags.push("--disable-lz4");
     }
 
-    configure_flags.push("--enable-static");
-
     println!("Configuring librdkafka");
     run_command_or_fail("librdkafka", "./configure", configure_flags.as_slice());
 
