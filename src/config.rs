ef388572ef7e319dde74affe992f89956b6f9567
Matt Usifer <mattusifer@gmail.com>
2024-01-21 08:11:40 -0500

    Add get_watermark_offsets

--- src/config.rs
+++ src/config.rs
@@ -23,7 +23,7 @@
 //! [librdkafka-config]: https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
 
 use std::collections::HashMap;
-use std::ffi::{CStr, CString};
+use std::ffi::CString;
 use std::iter::FromIterator;
 use std::os::raw::c_char;
 use std::ptr;
