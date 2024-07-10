05ba0a8ded7b9eaf5e0ab00e53b40960c064fd09
Maverick Liu <maverick.liu42@gmail.com>
2024-07-10 14:53:09 +0800

    Impl ToBytes for [u8; N] with generic const

--- src/message.rs
+++ src/message.rs
@@ -822,22 +822,10 @@ impl ToBytes for () {
     }
 }
 
-// Implement to_bytes for arrays - https://github.com/rust-lang/rfcs/issues/1038
-macro_rules! array_impls {
-    ($($N:expr)+) => {
-        $(
-            impl ToBytes for [u8; $N] {
-                fn to_bytes(&self) -> &[u8] { self }
-            }
-         )+
+impl<const N: usize> ToBytes for [u8; N] {
+    fn to_bytes(&self) -> &[u8] {
+        self
     }
-}
-
-array_impls! {
-     0  1  2  3  4  5  6  7  8  9
-    10 11 12 13 14 15 16 17 18 19
-    20 21 22 23 24 25 26 27 28 29
-    30 31 32
 }
 
 #[cfg(test)]
