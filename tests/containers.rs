2351d8fa435e970d915f6c2089c58a0209564182
Johnathan W <j7n.w4r@gmail.com>
2025-10-02 15:37:46 -0400

    Get bootstrap servers.

--- tests/containers.rs
+++ tests/containers.rs
@@ -1,5 +1,6 @@
 use anyhow::Context;
 use testcontainers_modules::kafka::apache::Kafka;
+use testcontainers_modules::testcontainers::core::ContainerPort;
 use testcontainers_modules::testcontainers::runners::AsyncRunner;
 use testcontainers_modules::testcontainers::ContainerAsync;
 
@@ -17,7 +18,16 @@ impl KafkaContext {
         Ok(Self { kafka_node })
     }
 
-    pub async fn bootstrap_servers(&self) -> String {
-        unimplemented!("not yet implemented")
+    pub async fn bootstrap_servers(&self) -> anyhow::Result<String> {
+        let kafka_host = self
+            .kafka_node
+            .get_host()
+            .await
+            .context("Failed to get Kafka host")?;
+        let kafka_port = self
+            .kafka_node
+            .get_host_port_ipv4(ContainerPort::Tcp(9092))
+            .await?;
+        Ok(format!("{}:{}", kafka_host.to_string(), kafka_port))
     }
 }
